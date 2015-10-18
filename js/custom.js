5c4abc28decb76c754827edb8e417a36d7a7cc90
Akshat Mahajan <akshatm.bkk@gmail.com>
2015-10-17 18:35:16 -0700

    Added Handlebars client-side example

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,7 @@
-$(document).ready(function() {
-  $.simpleWeather({
-    location: 'Los Angeles, CA',
-    woeid: '',
-    unit: 'c',
-    success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp;
-      html += ' <small>'+weather.city+'</small></h2>';
-      $("#weather").html(html);
-    },
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-  });
-});
+var source   = $("#entry-template").html();
+var template = Handlebars.compile(source);
+
+$("#news").html(template({
+    news: 'Dead Poets Society rocks on!',
+    source: 'Daily Bruin Online Feed'
+}));
