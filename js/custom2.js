dc61051e5af5f47e4fa6e52d7299b5a0dbada8f5
Akshat Mahajan <akshatm.bkk@gmail.com>
2015-10-23 00:37:38 -0700

    Added helper

--- js/custom2.js
+++ js/custom2.js
@@ -4,6 +4,36 @@ var source = $("#entry-template").html();
 var template = Handlebars.compile(source);
 var newsfeed = "http://crossorigin.me/http://dailybruin.com/feed";
 
+Handlebars.registerHelper('clean', function(content){
+    
+    /* 
+    
+    This helper parses content in description to reflect custom styling e.g.
+    untagged text -> text enclosed in <p> tags; <img> tags have custom styling
+    stripped from them and replaced
+    
+    */
+    
+    // To wrap untagged text, we must first insert it into its own HTML node,
+    // iterate through the contents, and wrap each content
+    
+    var cleanedContent = $('<div></div>');
+    cleanedContent.html(content);
+    
+    $(cleanedContent).contents().filter(function() {
+        return this.nodeType == 3; // for all text nodes 
+    }).wrap('<p class="article">'); // wrap them in <p> tags
+    
+    // remove all styling from images, and replace them with our own
+    
+    // implement elsewhere
+    
+    // Return as HTML
+    
+    return new Handlebars.SafeString($(cleanedContent).html());
+    
+});
+
 $(document).ready(function() {
 
     $.get(newsfeed,
@@ -11,29 +41,17 @@ $(document).ready(function() {
         
             var serialized_XML = new XMLSerializer().serializeToString(response);
             var json = XMLMapping.load(serialized_XML);
-            
-            console.log(json); // for your benefit! You can see what the object looks like in the console.
-
+    
             // get source template
             var templateSource = $("#entry-template").html();
             template = Handlebars.compile(templateSource);
 
-            // parse the data into the format we want
-            var data = json["rss"]["channel"]["item"][0];
+            //parse the data into the format we want
+            var data = json["rss"]["channel"]["item"];
 
-            var returndata = {
+            var returndata = {data: data};
 
-                url: data["link"]["$t"],
-
-                title: data["title"]["$t"],
-                
-                author: data["dc$creator"]["$cd"],
-                
-                content: data["description"]["$cd"]
-
-            };
-
-            // update the page with the formatted data
+            //update the page with the formatted data
             var newsHTML = template(returndata);
             $("#news").html(newsHTML);
 
